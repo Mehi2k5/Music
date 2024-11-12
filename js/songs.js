92e298576d1c9095d69fa1c0d8d063daf695974b
Xu <147921346+Mehi2k5@users.noreply.github.com>
2024-11-11 21:47:27 -0500

    Create songs.js

---
+++ js/songs.js
@@ -0,0 +1,270 @@
+/**
+ * songs.js
+ *
+ * The app's songs
+ */
+
+window.songs = [
+  
+  /* TODO */
+  {
+    songId: "SID-123-1",
+    artistId: "AID-123",
+    title: "Fur Elise",
+    album: "N/A",
+    year: "1867",
+    duration: 175,
+    url: "https://www.youtube.com/watch?v=wfF0zHeU3Zs",
+    explicit: false
+  },
+
+  { 
+    songId: "SID-123-2",
+    artistId: "AID-123",
+    title: "Beethoven Virus",
+    album: "Pathétique Sonata",
+    year: "2008",
+    duration: 220,
+    url: "https://www.youtube.com/watch?v=DtKCNJmARF0",
+    explicit: false
+  },
+
+  {
+    songId: "SID-456-1",
+    artistId: "AID-456",
+    title: "Turkish March ",
+    album: "A major, K. 331",
+    year: "1783",
+    duration: 241,
+    url: "https://www.youtube.com/watch?v=HMjQygwPI1c",
+    explicit: false
+  },
+
+  {
+    songId: "SID-123-3",
+    artistId: "AID-123",
+    title: "Moonlight Sonata",
+    album: "N/A",
+    year: "1801",
+    duration: 900,
+    url: "https://www.youtube.com/watch?v=4Tr0otuiQuU",
+    explicit: false
+  },
+
+  {
+    songId: "SID-123-4",
+    artistId: "AID-123",
+    title: "The Fifth.",
+    album: "N/A",
+    year: "1808",
+    duration: 2362,
+    url: "https://www.youtube.com/watch?v=-VVXqNt4qU0&t=274s",
+    explicit: false
+  },
+
+  //5
+
+
+
+  {
+    songId: "SID-123-5",
+    artistId: "AID-123",
+    title: "Pathétique",
+    album: "N/A",
+    year: "1798",
+    duration: 1130,
+    url: "https://www.youtube.com/watch?v=7eYktxvczjw",
+    explicit: false
+  },
+
+
+
+
+  {
+    songId: "SID-123-6",
+    artistId: "AID-123",
+    title: "Missa Solemnis",
+    album: "N/A",
+    year: "1823",
+    duration: 4611,
+    url: "https://www.youtube.com/watch?v=7eYktxvczjw",
+    explicit: true
+  },
+
+
+
+
+  {
+    songId: "SID-123-7",
+    artistId: "AID-123",
+    title: "Violin Concerto ",
+    album: "N/A",
+    year: "1806",
+    duration: 2828,
+    url: "https://www.youtube.com/watch?v=cokCgWPRZPg",
+    explicit: true
+  },
+
+
+  {
+    songId: "SID-123-8",
+    artistId: "AID-123",
+    title: "Emperor",
+    album: "N/A",
+    year: "1809",
+    duration: 2484,
+    url: "https://www.youtube.com/watch?v=hDXWK3W477w",
+    explicit: false
+  },
+
+
+  {
+    songId: "SID-123-9",
+    artistId: "AID-123",
+    title: "Pastoral",
+    album: "N/A",
+    year: "N/A",
+    duration: 649,
+    url: "https://www.youtube.com/watch?v=iMJPZ-mu-Ts",
+    explicit: false
+  },
+
+
+  //10
+
+
+  {
+    songId: "SID-456-2",
+    artistId: "AID-456",
+    title: "Symphony No. 40 in G minor",
+    album: "Mozart: The Complete Symphonies",
+    year: "1788",
+    duration: 1585,
+    url: "https://www.youtube.com/watch?v=JTc1mDieQI8",
+    explicit: false
+  },
+
+  {
+    songId: "SID-456-3",
+    artistId: "AID-456",
+    title: "Eine kleine Nachtmusik",
+    album: "Mozart: Serenades",
+    year: "1787",
+    duration: 347,
+    url: "https://www.youtube.com/watch?v=oy2zDJPIgwc",
+    explicit: false
+  },
+
+
+  {
+    songId: "SID-456-4",
+    artistId: "AID-456",
+    title: "Piano Sonata No. 11 in A major, K. 331",
+    album: "Mozart: The Piano Sonatas",
+    year: "1783",
+    duration: 213,
+    url: "https://www.youtube.com/watch?v=SQh1zztmpEk",
+    explicit: false
+  },
+
+  {
+    songId: "SID-456-5",
+    artistId: "AID-456",
+    title: "Requiem in D minor",
+    album: "Mozart: Requiem",
+    year: "1791",
+    duration: 416,
+    url: "https://www.youtube.com/watch?v=qiMcXzfm9Mg",
+    explicit: false
+  },
+
+  {
+    songId: "SID-456-6",
+    artistId: "AID-456",
+    title: "Violin Concerto No. 5 in A major, K. 219",
+    album: "Mozart: The Violin Concertos",
+    year: "N/A",
+    duration: 2009,
+    url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    explicit: false
+  },
+
+
+
+//15
+
+  {
+    songId: "SID-789-1",
+    artistId: "AID-789",
+    title: "Toccata and Fugue in D minor",
+    album: "Bach: Organ Works ",
+    year: "1962",
+    duration: 512,
+    url: "https://www.youtube.com/watch?v=Nnuq9PXbywA",
+    explicit: false
+  },
+
+  {
+    songId: "SID-789-2",
+    artistId: "AID-789",
+    title: "Brandenburg Concerto No. 3 in G major",
+    album: "Bach: Brandenburg Concertos",
+    year: "1964",
+    duration: 702,
+    url: "https://www.youtube.com/watch?v=pdsyNwUoON0",
+    explicit: false
+  },
+
+  {
+    songId: "SID-789-3",
+    artistId: "AID-789",
+    title: "Cello Suite No. 1 in G major",
+    album: "Bach: Cello Suites",
+    year: "1997",
+    duration: 195,
+    url: "https://www.youtube.com/watch?v=1prweT95Mo0",
+    explicit: false
+  },
+
+
+  {
+    songId: "SID-789-4",
+    artistId: "AID-789",
+    title: "Jesu, Joy of Man's Desiring",
+    album: "Bach: The Essential Collection",
+    year: "2000",
+    duration: 225,
+    url: "https://www.youtube.com/watch?v=wEJruV9SPao",
+    explicit: false
+  },
+
+
+  {
+    songId: "SID-789-5",
+    artistId: "AID-789",
+    title: "Mass in B minor",
+    album: "Bach: Mass in B minor",
+    year: "1977",
+    duration: 6003,
+    url: "https://www.youtube.com/watch?v=7F7TVM8m95Y",
+    explicit: false
+  },
+
+  
+  {
+    songId: "SID-789-6",
+    artistId: "AID-789",
+    title: "Prelude and Fugue in C major, BWV 846",
+    album: " Bach: The Well-Tempered Clavier ",
+    year: "1955",
+    duration: 262,
+    url: "https://www.youtube.com/watch?v=mKsAypz6Ou8",
+    explicit: false
+  },
+
+
+    
+
+
+
+];
