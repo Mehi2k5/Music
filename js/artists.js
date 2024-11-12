81b8a79415aa1de34c915919d0bbfdc099497a42
Xu <147921346+Mehi2k5@users.noreply.github.com>
2024-11-11 21:47:45 -0500

    Create artists.js

---
+++ js/artists.js
@@ -0,0 +1,41 @@
+/**
+ * artists.js
+ *
+ * The app's list of Artists
+ */
+
+window.artists = [
+
+
+  /* TODO */
+  {
+    artistId: "AID-123",
+    name: "Ludwig Van Beethoven",
+    urls: [
+    { url: "https://open.spotify.com/artist/2wOqMjp9TyABvtHdOSOTUS", name: "Spotify" },
+    { url: "https://soundcloud.com/ludwigvanbeethovenofficial", name: "Soundcloud" }   
+    ]
+  },
+
+  {
+    artistId: "AID-456",
+    name: "Wolfgang Amadeus Mozart",
+    urls: [
+    { url: "https://open.spotify.com/artist/4NJhFmfw43RLBLjQvxDuRS", name: "Spotify" },
+    { url: "https://soundcloud.com/mozart", name: "Soundcloud" }   
+    ]
+  },
+  
+  
+  {
+    artistId: "AID-789",
+    name: "Johann Sebastian Bach",
+    urls: [
+    { url: "https://open.spotify.com/artist/5aIqB5nVVvmFsvSdExz408", name: "Spotify" },
+    { url: "https://soundcloud.com/johann-sebastian-bach-official", name: "Soundcloud" }   
+    ]
+  }
+
+
+
+];
